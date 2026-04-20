ce7e3a30a8b92056495aaa1a72f3250636a7bc47
HarshSingla <harshsingla2907@gmail.com>
2026-04-21 00:57:08 +0530

    Added CI/CD pipeline

---
+++ customer_service/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+customers = {
+    1: {"name": "Abhinav", "orders": [101, 102]},
+    2: {"name": "Rahul", "orders": [103]}
+}
+
+@app.route('/customers/<int:customer_id>/orders', methods=['GET'])
+def get_customer_orders(customer_id):
+    customer = customers.get(customer_id)
+
+    if not customer:
+        return jsonify({"error": "Customer not found"}), 404
+
+    return jsonify({
+        "customer_id": customer_id,
+        "name": customer["name"],
+        "orders": customer["orders"]
+    })
+
+# 🔥 IMPORTANT FOR RENDER
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render uses dynamic port
+    app.run(host="0.0.0.0", port=port)
